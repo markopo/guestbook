fb05f47db094445d535c8861f50885e416969d27
Marko <marko_poikkimaki@hotmail.com>
2020-07-23 13:00:23 +0200

    add redis cache

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,15 +6,20 @@ use App\Entity\Conference;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class ConferenceController extends AbstractController
 {
     private ConferenceRepository $conferenceRepository;
 
+    private AdapterInterface $cache;
 
-    public function __construct(ConferenceRepository $conferenceRepository)
+
+    public function __construct(ConferenceRepository $conferenceRepository,
+                                AdapterInterface $cache)
     {
         $this->conferenceRepository = $conferenceRepository;
+        $this->cache = $cache;
     }
 
 
@@ -23,9 +28,21 @@ class ConferenceController extends AbstractController
      */
     public function index()
     {
+        $item = $this->cache->getItem('testCacheKey');
+        $itemFromCache = true;
+
+        if(!$item->isHit()) {
+            $itemFromCache = false;
+            $item->set('bla bla');
+            $item->expiresAfter(new \DateInterval('PT10S'));
+            $this->cache->save($item);
+        }
+
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
-            'conferences' => $this->conferenceRepository->findAll()
+            'conferences' => $this->conferenceRepository->findAll(),
+            'isCached' => $itemFromCache ? 'true' : 'false',
+            'now' => date('Y-m-d H:i:s')
         ]);
     }
 
